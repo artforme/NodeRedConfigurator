1d5fa5879141ec88ea93d9984ada280dd433028b
artforme <me.peremy@gmail.com>
2025-02-21 12:34:36 +0300

    feat: update MainWindow logic with new window interactions

---
+++ Presentation/Views/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using Infrastructure.Managers;
+
+namespace Presentation.Views;
+
+public partial class MainWindow : Window
+{
+    private readonly ConfigManager _configManager = new ConfigManager("templates.json");
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OpenSettings_Click(object sender, RoutedEventArgs e)
+    {
+        var settingsWindow = new SettingsWindow(_configManager);
+        settingsWindow.ShowDialog();
+    }
+    
+    private void OpenSelectionWindow_Click(object sender, RoutedEventArgs e)
+    {
+        var selectionWindow = new ChainSelectionWindow();
+        selectionWindow.ShowDialog();
+    }
+}
