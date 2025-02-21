4b0c3f76a1fc21b551448c5608b0cf76b80ec9ca
artforme <me.peremy@gmail.com>
2025-02-21 12:31:23 +0300

    feat: add ParameterTemplateSelector for dynamic template selection

---
+++ Presentation/Helpers/ParameterTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Presentation.ViewsModels;
+
+namespace Presentation.Helpers;
+
+public class ParameterTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate TextTemplate { get; set; }
+    public DataTemplate NumberTemplate { get; set; }
+    public DataTemplate BooleanTemplate { get; set; }
+    public DataTemplate DropdownTemplate { get; set; }
+
+    public override DataTemplate SelectTemplate(object item, DependencyObject container)
+    {
+        if (item is ChainParameterViewModel param)
+        {
+            return param.Type switch
+            {
+                "Text" => TextTemplate,
+                "Number" => NumberTemplate,
+                "Boolean" => BooleanTemplate,
+                "Dropdown" => DropdownTemplate,
+                _ => base.SelectTemplate(item, container)
+            };
+        }
+        return base.SelectTemplate(item, container);
+    }
+}
