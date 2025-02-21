f618c5b1049b193e36b7b1af39b0278186f129d1
artforme <me.peremy@gmail.com>
2025-02-21 12:33:34 +0300

    feat: introduce MainViewModel for main application data

---
+++ Presentation/ViewsModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Presentation.ViewsModels;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    public ObservableCollection<ChainViewModel> Chains { get; set; } = new();
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
