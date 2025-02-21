57b0e21289035497b7d6d13eb191c4510733d81b
artforme <me.peremy@gmail.com>
2025-02-21 12:32:19 +0300

    feat: introduce ChainParameterViewModel for parameter representation

---
+++ Presentation/ViewsModels/ChainParameterViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Presentation.ViewsModels;
+
+public class ChainParameterViewModel
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public object Value { get; set; }
+    public string[] Options { get; set; }
+
+    public ChainParameterViewModel(string name, string type, string[] options = null)
+    {
+        Name = name;
+        Type = type;
+        Options = options;
+    }
+}
