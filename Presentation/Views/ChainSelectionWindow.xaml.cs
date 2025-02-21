2fb533394a750f6a60b1a5582f9ccb1db0b8bd74
artforme <me.peremy@gmail.com>
2025-02-21 12:35:07 +0300

    feat: initialize ChainSelectionWindow with ChainSelectionViewModel

---
+++ Presentation/Views/ChainSelectionWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Presentation.ViewsModels;
+
+namespace Presentation.Views;
+
+public partial class ChainSelectionWindow : Window
+{
+    public ChainSelectionWindow()
+    {
+        InitializeComponent();
+        DataContext = new ChainSelectionViewModel();
+    }
+}
