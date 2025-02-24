a1a9f1eac8c58e999ed06041a9b3022753f965b6
artforme <me.peremy@gmail.com>
2025-02-24 18:44:20 +0300

    feat: add method that loads templates

--- Presentation/ViewsModels/MainViewModel.cs
+++ Presentation/ViewsModels/MainViewModel.cs
@@ -1,11 +1,36 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using Infrastructure.Managers;
 
 namespace Presentation.ViewsModels;
 
 public class MainViewModel : INotifyPropertyChanged
 {
-    public ObservableCollection<ChainViewModel> Chains { get; set; } = new();
+    private readonly ConfigManager _configManager;
+
+    public ObservableCollection<SettingsChainViewModel> Chains { get; set; } = new();
+
+    public MainViewModel(ConfigManager configManager)
+    {
+        _configManager = configManager ?? throw new ArgumentNullException(nameof(configManager));
+    }
+
+    public void LoadTemplates()
+    {
+        Chains.Clear();
+        foreach (var chainType in _configManager.GetAllChainTypes())
+        {
+            var alicePath = _configManager.GetTemplatePath(chainType, "Alice");
+            var applePath = _configManager.GetTemplatePath(chainType, "Apple");
+
+            Chains.Add(new SettingsChainViewModel(_configManager)
+            {
+                Type = chainType,
+                AlicePath = string.IsNullOrEmpty(alicePath) ? "Путь не задан" : alicePath,
+                ApplePath = string.IsNullOrEmpty(applePath) ? "Путь не задан" : applePath
+            });
+        }
+    }
 
     public event PropertyChangedEventHandler PropertyChanged;
 
