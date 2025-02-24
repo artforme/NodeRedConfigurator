00f45504ce4b27897cd2824a13ccabd665d1bfd7
artforme <me.peremy@gmail.com>
2025-02-24 18:41:50 +0300

    feat: update the view layer in the "SettingsWindow" class

--- Presentation/Views/SettingsWindow.xaml.cs
+++ Presentation/Views/SettingsWindow.xaml.cs
@@ -2,44 +2,26 @@
 using Infrastructure.Managers;
 using Presentation.ViewsModels;
 
-namespace Presentation.Views
+namespace Presentation.Views;
+
+public partial class SettingsWindow : Window
 {
-    public partial class SettingsWindow : Window
+    private readonly ConfigManager _configManager;
+    public MainViewModel ViewModel { get; }
+
+    public SettingsWindow(ConfigManager configManager)
     {
-        private readonly ConfigManager _configManager;
-        public MainViewModel ViewModel { get; }
+        InitializeComponent();
 
-        public SettingsWindow(ConfigManager configManager)
-        {
-            InitializeComponent();
+        _configManager = configManager ?? throw new ArgumentNullException(nameof(configManager));
+        ViewModel = new MainViewModel(_configManager);
+        DataContext = ViewModel;
 
-            _configManager = configManager;
+        LoadTemplates();
+    }
 
-            // Инициализация ViewModel
-            ViewModel = new MainViewModel();
-            DataContext = ViewModel;
-
-            LoadTemplates();
-        }
-        
-        private void LoadTemplates()
-        {
-            // Очищаем текущие цепи
-            ViewModel.Chains.Clear();
-
-            // Загружаем шаблоны из ConfigManager
-            foreach (var chainType in new[] { "Type1", "Type2", "Type3", "RgbStrip", "132", "141324132", "lighting", "flor", "somechain" }) // TODO: получить реальные типы
-            {
-                var alicePath = _configManager.GetTemplatePath(chainType, "Alice");
-                var applePath = _configManager.GetTemplatePath(chainType, "Apple");
-
-                ViewModel.Chains.Add(new ChainViewModel
-                {
-                    Type = chainType,
-                    AlicePath = string.IsNullOrEmpty(alicePath) ? "Путь не задан" : alicePath,
-                    ApplePath = string.IsNullOrEmpty(applePath) ? "Путь не задан" : applePath
-                });
-            }
-        }
+    private void LoadTemplates()
+    {
+        ViewModel.LoadTemplates();
     }
 }
