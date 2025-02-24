a165ee3a7e3c14869b1cd3420ad6c243a75d00e6
artforme <me.peremy@gmail.com>
2025-02-24 23:47:25 +0300

    feat: Implement chain management with unique IDs and events

--- Infrastructure/Managers/ChainManager.cs
+++ Infrastructure/Managers/ChainManager.cs
@@ -1,15 +1,28 @@
 ﻿using Models.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Infrastructure.Managers;
 
 public class ChainManager
 {
     private readonly Dictionary<Guid, Chain> _chains = new();
+    public event EventHandler<ChainEventArgs> ChainAdded; // Новое событие
 
     public Guid AddChain(Chain chain)
     {
-        var id = Guid.NewGuid();
+        if (chain == null)
+            throw new ArgumentNullException(nameof(chain));
+
+        // Используем Id, сгенерированный в chain (из конструктора)
+        Guid id = chain.Id;
+        if (_chains.ContainsKey(id))
+        {
+            throw new InvalidOperationException($"Chain with ID {id} already exists.");
+        }
         _chains[id] = chain;
+        OnChainAdded(new ChainEventArgs { ChainId = id, Chain = chain }); // Вызываем событие
         return id;
     }
 
@@ -35,7 +48,7 @@ public class ChainManager
     {
         if (!_chains.ContainsKey(id))
         {
-            throw new KeyNotFoundException($"Chain with ID {id} not found.");
+            throw new KeyNotFoundException($"Chain with ID {id} not found. Chains: {_chains.Count}");
         }
         return _chains[id];
     }
@@ -44,4 +57,16 @@ public class ChainManager
     {
         return _chains.Values;
     }
+
+    protected virtual void OnChainAdded(ChainEventArgs e)
+    {
+        ChainAdded?.Invoke(this, e);
+    }
+}
+
+// Класс аргументов события
+public class ChainEventArgs : EventArgs
+{
+    public Guid ChainId { get; set; }
+    public Chain Chain { get; set; }
 }
