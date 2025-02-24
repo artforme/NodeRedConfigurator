9e26b203ba8e730772541efe0b1ca41aa05c95cf
artforme <me.peremy@gmail.com>
2025-02-24 18:40:38 +0300

    feat: add config manager to settings view model

---
+++ Presentation/ViewsModels/SettingsChainViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using Microsoft.Win32;
+using Presentation.Helpers;
+
+namespace Presentation.ViewsModels;
+public class SettingsChainViewModel : INotifyPropertyChanged
+{
+    private readonly ConfigManager _configManager;
+    private string _alicePath;
+    private string _applePath;
+
+    public string Type { get; set; }
+
+    public string AlicePath
+    {
+        get => _alicePath;
+        set
+        {
+            _alicePath = value;
+            OnPropertyChanged(nameof(AlicePath));
+        }
+    }
+
+    public string ApplePath
+    {
+        get => _applePath;
+        set
+        {
+            _applePath = value;
+            OnPropertyChanged(nameof(ApplePath));
+        }
+    }
+
+    public ICommand SelectAlicePathCommand { get; }
+    public ICommand SelectApplePathCommand { get; }
+
+    public SettingsChainViewModel(ConfigManager configManager)
+    {
+        _configManager = configManager ?? throw new ArgumentNullException(nameof(configManager));
+        SelectAlicePathCommand = new RelayCommand(SelectAlicePath);
+        SelectApplePathCommand = new RelayCommand(SelectApplePath);
+    }
+
+    private void SelectAlicePath()
+    {
+        var dialog = new OpenFileDialog
+        {
+            Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*",
+            InitialDirectory = _configManager.TemplatesFolderPath.Value // Устанавливаем начальную папку templates
+        };
+        if (dialog.ShowDialog() == true)
+        {
+            AlicePath = dialog.FileName;
+            _configManager.SetTemplatePath(Type, "Alice", AlicePath); // Сохраняем путь в ConfigManager
+        }
+    }
+
+    private void SelectApplePath()
+    {
+        var dialog = new OpenFileDialog
+        {
+            Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*",
+            InitialDirectory = _configManager.TemplatesFolderPath.Value // Устанавливаем начальную папку templates
+        };
+        if (dialog.ShowDialog() == true)
+        {
+            ApplePath = dialog.FileName;
+            _configManager.SetTemplatePath(Type, "Apple", ApplePath); // Сохраняем путь в ConfigManager
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
