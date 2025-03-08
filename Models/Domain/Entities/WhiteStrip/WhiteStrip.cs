7571c8a4a1d17dedddd7b838110ee217ab818872
artforme <me.peremy@gmail.com>
2025-03-08 16:24:38 +0300

    feat: add new chain types

---
+++ Models/Domain/Entities/WhiteStrip/WhiteStrip.cs
@@ -0,0 +1,35 @@
+﻿using Models.Domain.ValueObjects;
+
+namespace Models.Domain.Entities.RgbStrip;
+
+public class WhiteStrip : Chain
+{
+    public WhiteStrip(Dictionary<string, object> properties) : base(new Info("WhiteStrip"), properties) { }
+
+    public override (string search, object replace)[] GetProperties()
+    {
+        var keyMapping = new Dictionary<string, string>
+        {
+            { "Name", "%%name%%" },
+            { "Device", "%%device%%" },
+            { "DevAddress", "%%dev_address%%" },
+            { "Room", "%%room%%" }
+        };
+
+        var propertiesList = new List<(string search, object replace)>();
+
+        foreach (var readableKey in keyMapping.Keys)
+        {
+            if (Properties.TryGetValue(readableKey, out var value))
+            {
+                propertiesList.Add((keyMapping[readableKey], value));
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Property '{readableKey}' not found in the properties dictionary.");
+            }
+        }
+
+        return propertiesList.ToArray();
+    }
+}
